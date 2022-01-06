46506da7f40d9c121478baf4f0c00a67160157b0
sobotapl <piosoboci@gmail.com>
2022-01-06 15:22:33 +0100

    1st task done

---
+++ src/main/java/qwerty/Main.java
@@ -0,0 +1,31 @@
+package qwerty;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj ciag znaków: ");
+        System.out.println(encode(sc.nextLine().toUpperCase()));
+
+    }
+
+    public static String fromMap(String letter){
+        return Quwerty.map.getOrDefault(letter,"");
+    }
+
+
+    public static String encode(String text) {
+        String result ="";
+        if(Objects.nonNull(text)){
+            for(int i = 0; i <text.length(); i++){
+                String letter = String.valueOf(text.charAt(i));
+                result += fromMap(letter) + " ";
+            }
+        }
+        return result;
+    }
+
+}
